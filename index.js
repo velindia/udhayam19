808aef7f72d1b5112dbcb9cb95273592f6afe2f8
velindia <velindia502@gmail.com>
2019-02-19 16:25:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.listen(4000||process.env.PORT);
+console.log('4000 is the magic port');
